5b57fca2cb44f13c926e3918442b0bc0e6490f0f
analuthfa <luthfa.ana@gmail.com>
2020-02-21 09:35:32 +0700

    display product

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', 'Ecommerce\FrontController@index')->name('front.index');
+Route::get('/product', 'Ecommerce\FrontController@product')->name('front.product');
+
+Auth::routes(); //ROUTING INI MENCAKUP SEMUA ROUTING YANG BERKAITAN DENGAN AUTHENTICATION
+
+//JADI INI GROUPING ROUTE, SEHINGGA SEMUA ROUTE YANG ADA DIDALAMNYA
+//SECARA OTOMATIS AKAN DIAWALI DENGAN administrator
+//CONTOH: /administrator/category ATAU /administrator/product, DAN SEBAGAINYA
+Route::group(['prefix' => 'administrator', 'middleware' => 'auth'], function() {
+    Route::get('/home', 'HomeController@index')->name('home'); //JADI ROUTING INI SUDAH ADA DARI ARTIKEL SEBELUMNYA TAPI KITA PINDAHKAN KEDALAM GROUPING
+
+    //INI ADALAH ROUTE BARU
+    Route::resource('category', 'CategoryController')->except(['create', 'show']);
+        Route::get('/category', 'CategoryController@index')->name('category.index');
+        Route::post('/category', 'CategoryController@store')->name('category.store');
+        Route::get('/category/{category_id}/edit', 'CategoryController@edit')->name('category.edit');
+        Route::put('/category/{category_id}', 'CategoryController@update')->name('category.update');
+        Route::delete('/category/{category_id}', 'CategoryController@destroy')->name('category.destroy');   
+
+    Route::resource('product', 'ProductController')->except(['show']); //BAGIAN INI KITA TAMBAHKAN EXCEPT KARENA METHOD SHOW TIDAK DIGUNAKAN
+        Route::get('/product/bulk', 'ProductController@massUploadForm')->name('product.bulk'); //TAMBAHKAN ROUTE INI
+        Route::post('/product/bulk', 'ProductController@massUpload')->name('product.saveBulk');
+        
+});
+
